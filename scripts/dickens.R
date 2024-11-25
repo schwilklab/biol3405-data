f48a2a100035c6e43ee4d97a9ee083f34f0c8c47
Azaj Mahmud <80069081+Azajmahmud@users.noreply.github.com>
2024-11-25 15:32:44 -0600

    Modified dickens.R
    Read and merged the pv_leaf_dickens data with wp,species and leaf area
    data

--- scripts/dickens.R
+++ scripts/dickens.R
@@ -15,6 +15,7 @@ species <- read_csv("data/dickens/species.csv")
 #species <- read.csv("data/dickens/species.csv", stringsAsFactors=FALSE)
 
 leaves <- read_csv("data/dickens/leaf_area.csv")
+pv_leaves <- read_csv("data/dickens/pv_leaf_dickens.csv")
 
 ## Calculate LMA and leaf size trait:
 leaves <- mutate(leaves, LMA=dry_mass/leaf_area, leaf_size=leaf_area/n_leaves)
@@ -33,14 +34,32 @@ nrow(dickens)
 ## print column names:
 names(dickens)
 
+########################################################
+# Selecting the traits from pv_leaves that doesn't overlap with
+# leaves and merge with dickens by spcode
+#################################################################
+
+pv_leaves <- pv_leaves %>%
+  dplyr::select(- c("lma", "leaf_area"))
+
+dickens <- left_join(dickens, pv_leaves, by = "spcode")
+
 ## Summarize water potential and trait values by genus. This is the same as
 ## sumamrizing by species because we have no genera with more than one species
 ## here.
 by_genus <- group_by(dickens, genus)
 genus_means <- summarize(by_genus, mean_pd=mean(predawn_wp), mean_md=mean(midday_wp),
-          mean_lsize = mean(leaf_size, na.rm=TRUE), mean_LMA = mean(LMA, na.rm=TRUE))
-
-
+          mean_lsize = mean(leaf_size, na.rm=TRUE), mean_LMA = mean(LMA, na.rm=TRUE),
+          mean_ldmc = mean(ldmc, na.rm = TRUE),
+          leaf_length = mean(leaf_length, na.rm = TRUE),
+          tlp = mean(tlp, na.rm = TRUE),
+          rwc_tlp = mean(rwc_tlp, na.rm = TRUE),
+          capacitance_above_tlp = mean(capacitance_above_tlp, na.rm = TRUE),
+          capacitance_below_tlp = mean(capacitance_below_tlp, na.rm = TRUE),
+          modulus_elasticity = mean(modulus_elasticity, na.rm = TRUE),
+          swc = mean(swc, na.rm = TRUE),
+          osmotic_potential = mean(osmotic_potential, na.rm = TRUE))
+          
 
 # Water potential figures
 ggplot(dickens, aes(predawn_wp, midday_wp, color=genus)) +
